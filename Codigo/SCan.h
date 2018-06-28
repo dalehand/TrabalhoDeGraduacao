abf55162c03016759f9d251a7ecfdccfca61e311
dalehand <freewill.jun@gmail.com>
2018-06-27 21:46:28 -0300

    Atualizado

--- Codigo/SCan.h
+++ Codigo/SCan.h
@@ -13,11 +13,13 @@
 #include<cstdint>
 #include<cstdio>
 #include<cstring>
+#include "DeviceNet.h"
 
 int write_flag = 0;
 int read_flag = 0;
 int sock_can_on = 1;
 UINT can_id_write;
+
 UCHAR global_CAN_write[BUFSIZE];
 
 UCHAR global_CAN_buf[BUFSIZE];
@@ -36,7 +38,7 @@ void strcpy_UCHAR (UCHAR *dst, char *src)
   dst[i]='\0';
 }
 
-void sock_can()
+void sock_can_read()
 {
   struct ifreq ifr;
   struct sockaddr_can addr;
@@ -56,8 +58,10 @@ void sock_can()
   addr.can_family = PF_CAN;
 
   bind(s, (struct sockaddr *)&addr, sizeof(addr));
+
   while(sock_can_on)
     {
+
       read(s, &frame, sizeof(frame));
       valid_message = 0;
 
@@ -87,23 +91,50 @@ void sock_can()
                 global_event |= 0x0020;
 		valid_message = 1;
                 break;
-            case 0b10111100111: // Duplicate MAC ID Check Messages: Group 2 MACID = 60 Message ID = 7
-                global_event |= 0x0040;
-		valid_message = 1;
-                break;
+		//case 0b10111100111: // Duplicate MAC ID Check Messages: Group 2 MACID = 60 Message ID = 7
+                //global_event |= 0x0040;
+		//valid_message = 1;
+                //break;
 	    default:
 	        valid_message = 0;
 		break;
         }
-      
       if(valid_message)
       {
-      for(count=0;count<candlc;count++)
+      global_CAN_buf[LENGTH] = frame.can_dlc;
+      printf("%X\t",frame.can_id);
+      for(count=0;count<global_CAN_buf[LENGTH];count++)
 	{
 	  global_CAN_buf[count]=frame.data[count];
+	  printf("%02X ",global_CAN_buf[count]);
 	}
-      global_CAN_buf[LENGTH] = frame.can_dlc;
       }
+    }
+  close(s);
+}
+
+void sock_can_write()
+{
+
+  struct ifreq ifr;
+  struct sockaddr_can addr;
+  struct can_frame frame;
+  int s;
+  int count = 0;
+  memset(&ifr, 0x0, sizeof(ifr));
+  memset(&addr, 0x0, sizeof(addr));
+  memset(&frame, 0x0, sizeof(frame));
+
+  s = socket(PF_CAN, SOCK_RAW, CAN_RAW);
+  strcpy(ifr.ifr_name, "can0");
+  ioctl(s, SIOCGIFINDEX, &ifr);
+  
+  addr.can_ifindex = ifr.ifr_ifindex;
+  addr.can_family = PF_CAN;
+
+  bind(s, (struct sockaddr *)&addr, sizeof(addr));
+  while(1)
+    {
       if(write_flag)
 	{
 	  frame.can_id = can_id_write;
@@ -116,6 +147,30 @@ void sock_can()
 	  write(s,&frame,sizeof(frame));
 	  write_flag=0;
 	}
-      }
+    }
   close(s);
+}
+
+void timer_func()
+{
+  if (global_timer[EXPLICIT])            // Explicit Connection timer
+    {
+      global_timer[EXPLICIT]--;           // decrement until zero
+      if (global_timer[EXPLICIT] == 0) global_event |= EXPLICIT_TIMEOUT;
+    }
+  if (global_timer[IO_POLL])             // I/O Poll Connection timer
+    {
+      global_timer[IO_POLL]--;            // decrement until zero
+      if (global_timer[IO_POLL] == 0) global_event |= IO_POLL_TIMEOUT;
+    }
+  if (global_timer[ACK_WAIT])          	// Acknowlege message timer
+    {
+      global_timer[ACK_WAIT]--;   	      // decrement until zero
+      if (global_timer[ACK_WAIT] == 0) global_event |= ACK_WAIT_TIMEOUT;
+    }
+  if (global_timer[UPDATE])              // Timer for updating the device
+    {
+      global_timer[UPDATE]--;   	         // decrement until zero
+      if (global_timer[UPDATE] == 0) global_event |= DEVICE_UPDATE;
+    }
 }
